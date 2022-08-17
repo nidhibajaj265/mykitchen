9b9fa1ab358ba8d31ff2ef49b2d67ea5ff8f2fe1
nidhibajaj265 <108235835+nidhibajaj265@users.noreply.github.com>
2022-08-17 18:52:59 +0530

    Initial Commit

---
+++ src/main/java/com/tw/myKitchen/MyKitchenApplication.java
@@ -0,0 +1,13 @@
+package com.tw.myKitchen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyKitchenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyKitchenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tw/myKitchen/MyKitchenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tw.myKitchen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyKitchenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
