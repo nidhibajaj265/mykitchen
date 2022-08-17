ef8762843bbadf2a063b6b3b031cd15bbd94b816
nidhibajaj265 <nidi.bajaj@gmail.com>
2022-08-17 22:07:05 +0530

    added entity class MenuItem.java

---
+++ src/main/java/com/tw/myKitchen/model/MenuItem.java
@@ -0,0 +1,31 @@
+package com.tw.myKitchen.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table (name = "menu_items")
+
+public class MenuItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private long id;
+    @Column(name = "name", nullable = false)
+    private String name;
+    @Column (name = "price", nullable = false)
+    private double price;
+    @Column (name = "type", nullable = false)
+    private String type;
+}
