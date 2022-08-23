157c5e613e245ca53e887251c8890dfd5c665f9d
nidhibajaj265 <nidi.bajaj@gmail.com>
2022-08-23 08:02:05 +0530

    changed config.yml to fix build issues, removed unused import from app.js

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -4,20 +4,19 @@ import {Container, Row} from 'react-bootstrap'
 import Welcome from './components/Welcome'
 import NavigationBar from './components/NavigationBar';
 import Footer from './components/Footer';
-import ReactDOM from 'react-dom';
 import AddMenuItem from './components/AddMenuItem';
 import ListMenuItems from './components/ListMenuItems';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import background from './background.png';
 
 function App() {
-  const myStyle={
-    backgroundImage: "url(/background.png)",
-    height:'100vh',
-    marginTop:'-70px',
-    fontSize:'50px',
-    backgroundSize: 'cover',
-  }
+  // const myStyle={
+  //   backgroundImage: "url(/background.png)",
+  //   height:'100vh',
+  //   marginTop:'-70px',
+  //   fontSize:'50px',
+  //   backgroundSize: 'cover',
+  // }
   return (
     <Router>
         <NavigationBar/>
